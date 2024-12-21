c0362c15480670ce31fc54957255916a26e3bb4c
nocturnal_hum <thanin.chan@gmail.com>
2024-12-21 18:43:50 -0500

    Dynamically import 'p5' to fix SSR Issues

--- app/components/PacmanGame.tsx
+++ app/components/PacmanGame.tsx
@@ -7,40 +7,46 @@ const PacmanGame: React.FC = () => {
   const sketchRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const sketch = (p: p5) => {
-      const pacmanX = 300;
-      const pacmanY = 300;
-      const pacmanSize = 50;
+    // Dynamically import p5 to avoid SSR issues
+    import('p5').then((p5) => {
+      const sketch = (p: p5) => {
+        const pacmanX = 300;
+        const pacmanY = 300;
+        const pacmanSize = 50;
 
-      p.setup = () => {
-        p.createCanvas(1000, 1000);
+        p.setup = () => {
+          p.createCanvas(1000, 1000);
+        };
+
+        p.draw = () => {
+          p.background(10);
+
+          // Draw Pac-man
+          p.fill(255, 255, 0);
+
+          p.arc(
+            pacmanX,
+            pacmanY,
+            pacmanSize,
+            pacmanSize,
+            p.radians(30),
+            p.radians(-30)
+          );
+        };
       };
 
-      p.draw = () => {
-        p.background(10);
+      // Check if sketchRef.current is not null before passing it
+      if (sketchRef.current) {
+        const canvas = new p5.default(sketch, sketchRef.current);
 
-        // Draw Pac-man
-        p.fill(255, 255, 0);
-
-        p.arc(
-          pacmanX,
-          pacmanY,
-          pacmanSize,
-          pacmanSize,
-          p.radians(30),
-          p.radians(-30)
-        );
-      };
-    };
-
-    const canvas = new p5(sketch, sketchRef.current);
-
-    // Cleanup the p5 instance when the component unmounts
-    return () => {
-      if (canvas) {
-        canvas.remove();
+        // Cleanup the p5 instance when the component unmounts
+        return () => {
+          if (canvas) {
+            canvas.remove();
+          }
+        };
       }
-    };
+    });
   }, []);
 
   return <div ref={sketchRef} />;
